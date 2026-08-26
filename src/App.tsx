047106df1b2fcc76376b65cbcdfd6f20e6020884
Gabriel Souza <gabrielsouza65115@gmail.com>
2026-08-25 23:23:19 -0300

    Data in localStorage

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import type { Product } from "./types/Product";
 import {
   House,
@@ -14,19 +14,25 @@ import {
   Camera,
   CameraIcon,
   ChevronDown,
+  SlidersHorizontal,
+  ArrowBigDown,
+  ShoppingBag,
+  ChevronLeft,
+  ChevronRight,
 } from "lucide-react";
+import { AreaChart, Area, ResponsiveContainer } from "recharts";
 
 function App() {
-  const [orcamento, setOrcamento] = useState(200);
-  const [produtos, setProdutos] = useState<Product[]>([
-    // {
-    //   id: 1,
-    //   nome: "Carne Bovina",
-    //   preco: 51.48,
-    //   quantidade: 1,
-    //   unidade: "Kg",
-    // },
-  ]);
+  const [orcamento, setOrcamento] = useState<number>(() => {
+    const orcamentoSalvo = localStorage.getItem("orcamentoAdd");
+
+    return orcamentoSalvo ? JSON.parse(orcamentoSalvo) : 200;
+  });
+  const [produtos, setProdutos] = useState<Product[]>(() => {
+    const ProdutosSalvos = localStorage.getItem("produtos");
+
+    return ProdutosSalvos ? JSON.parse(ProdutosSalvos) : [];
+  });
 
   const itensRodape = [
     {
@@ -76,8 +82,8 @@ function App() {
       !nomeProduto ||
       !precoProduto ||
       !quantidadeProduto ||
-      !unidadeProduto ||
-      !fotoProduto
+      !unidadeProduto
+      // !fotoProduto
     ) {
       alert("Preencha todos os campos para salvar o Produto");
       return;
@@ -89,7 +95,7 @@ function App() {
       preco: Number(precoProduto),
       quantidade: Number(quantidadeProduto),
       unidade: unidadeProduto,
-      imagem: fotoProduto,
+      // imagem: fotoProduto,
       dataCompra: new Date().toISOString(),
     };
 
@@ -109,9 +115,7 @@ function App() {
 
   // Função para deletar item
   function removerProduto(id: number) {
-    const confirm = window.confirm(
-      `Deseja realmente apagar este ${produtos.concat.name}`,
-    );
+    const confirm = window.confirm(`Deseja realmente apagar este Produto?`);
 
     if (confirm) {
       setProdutos((produtosAtuais) =>
@@ -136,6 +140,67 @@ function App() {
     setOrcamento(valor);
     setModalOrcamento(false);
   }
+
+  // Gráfico
+  const dados = [
+    { valor: 20 },
+    { valor: 45 },
+    { valor: 30 },
+    { valor: 50 },
+    { valor: 65 },
+    { valor: 52 },
+    { valor: 75 },
+    { valor: 100 },
+  ];
+
+  // Visualizar Histórico
+  const [abaAtiva, setAbaAtiva] = useState<"compras" | "resumo">("compras");
+
+  const compras = [
+    {
+      id: 1,
+      titulo: "Compra do dia",
+      data: "18/08/2026",
+      itens: 7,
+      valor: 73.5,
+    },
+    {
+      id: 2,
+      titulo: "Compra do dia",
+      data: "14/08/2026",
+      itens: 5,
+      valor: 125.2,
+    },
+    {
+      id: 3,
+      titulo: "Compra do dia",
+      data: "10/08/2026",
+      itens: 8,
+      valor: 236.4,
+    },
+  ];
+
+  // Persistência de Dados
+  useEffect(() => {
+    const produtosSalvos = localStorage.getItem("produtos");
+    const orcamentoSalvo = localStorage.getItem("orcamentoAdd");
+
+    if (produtosSalvos) {
+      setProdutos(JSON.parse(produtosSalvos));
+    }
+
+    if (orcamentoSalvo) {
+      setOrcamento(JSON.parse(orcamentoSalvo));
+    }
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem("produtos", JSON.stringify(produtos));
+  }, [produtos]);
+
+  useEffect(() => {
+    localStorage.setItem("orcamentoAdd", JSON.stringify(orcamento));
+  }, [orcamento]);
 
   return (
     <div className="min-h-screen ">
@@ -263,12 +328,6 @@ function App() {
                       <p className="text-md">
                         qtd: {produto.quantidade} {produto.unidade}
                       </p>
-                      <p className="text-sm text-gray-500">
-                        Comprado em:{" "}
-                        {new Date(produto.dataCompra).toLocaleDateString(
-                          "pt-BR",
-                        )}
-                      </p>
                     </div>
 
                     <div className="flex flex-col justify-between items-end">
@@ -285,7 +344,6 @@ function App() {
                 </div>
               ))}
             </section>
-
             <button
               className="bg-green-700 py-2.5 gap-2 rounded-xl w-full text-white flex justify-center mt-6 mb-24"
               onClick={() => setButtonAddItem(true)}
@@ -295,7 +353,7 @@ function App() {
               </span>{" "}
               <p>Adicionar produto</p>
             </button>
-
+            {/* Add produto */}
             {buttonAddItem && (
               <div className="fixed inset-0 overflow-y-auto p-6 bg-white">
                 <div className="flex justify-between ">
@@ -317,7 +375,6 @@ function App() {
                       if (arquivo) {
                         setFotoProdutos(URL.createObjectURL(arquivo));
                       }
-                      // console.log(arquivo);
                     }}
                   />
 
@@ -421,15 +478,138 @@ function App() {
                     className="bg-green-700 py-2.5 gap-2 rounded-xl w-full text-white flex justify-center mt-10 mb-24"
                     onClick={salvarProduto}
                   >
-                    {/* <span>
-                    <Plus />
-                  </span>{" "} */}
                     <p>Salvar produto</p>
                   </button>
                 </div>
               </div>
             )}
           </div>
+
+          {/*  Histórico de Compras */}
+          {/* <div>
+            <div className="flex justify-between">
+              <Menu />{" "}
+              <h1 className="text-lg font-medium">Histórico de compras</h1>{" "}
+              <SlidersHorizontal />
+            </div>
+
+            <div className="flex w-fit justify-between bg-green-100 rounded-full py-1.5 px-2 gap-4 items-center mx-auto mt-4">
+              <p className="text-sm text-green-700">Agosto de 2026</p>{" "}
+              <span>
+                <ChevronDown size={16} className="text-green-700" />
+              </span>
+            </div>
+
+            <div className="p-4 flex rounded-xl border border-t-0 border-gray-200 bg-white shadow-lg shadow-gray-300/30">
+              <div className="flex flex-col gap-3 w-[50%]">
+                <section>
+                  <h1 className="text-[12px]">Total gasto no mês</h1>
+                  <p className="text-[23px] text-green-700 font-medium">
+                    R$ 847,30
+                  </p>
+                </section>
+                <section>
+                  <h1 className="text-[12px]">Compras realizadas</h1>
+                  <p className="font-medium">12</p>
+                </section>
+              </div>
+              <div className="w-[60%]">
+                <div className="w-full h-full rounded-2xl border border-gray-200 bg-white p-3">
+                  <ResponsiveContainer width="100%" height="100%">
+                    <AreaChart data={dados}>
+                      <defs>
+                        <linearGradient
+                          id="corGrafico"
+                          x1="0"
+                          y1="0"
+                          x2="0"
+                          y2="1"
+                        >
+                          <stop
+                            offset="0%"
+                            stopColor="#2f6b4f"
+                            stopOpacity={0.15}
+                          />
+                          <stop
+                            offset="100%"
+                            stopColor="#2f6b4f"
+                            stopOpacity={0}
+                          />
+                        </linearGradient>
+                      </defs>
+
+                      <Area
+                        type="monotone"
+                        dataKey="valor"
+                        stroke="#2f6b4f"
+                        strokeWidth={2}
+                        fill="url(#corGrafico)"
+                        dot={false}
+                        activeDot={false}
+                      />
+                    </AreaChart>
+                  </ResponsiveContainer>
+                </div>
+              </div>
+            </div>
+
+            <div className="mt-2">
+              <div className="flex justify-between">
+                <button
+                  onClick={() => setAbaAtiva("compras")}
+                  className={`p-3 text-[12px] w-42 ${abaAtiva === "compras" ? "text-green-700 border-b-2 border-green-700" : "text-gray-500"}`}
+                >
+                  Compras
+                </button>
+                <button
+                  onClick={() => setAbaAtiva("resumo")}
+                  className={`p-3 text-[12px] w-42 ${abaAtiva === "resumo" ? "text-green-700 border-b-2 border-green-700" : "text-gray-500"}`}
+                >
+                  Resumo por categoria
+                </button>
+              </div>
+              <div></div>
+            </div>
+          </div>
+
+          <div className="space-y-3 mt-4">
+            {compras.map((compra) => (
+              <div
+                key={compra.id}
+                className="flex items-center justify-between rounded-xl bg-white p-4 border border-gray-200"
+              >
+                <div className="flex items-center gap-3">
+                  <div className="flex h-10 w-10 items-center justify-center rounded-full bg-green-100">
+                    <ShoppingBag className="text-green-700" />
+                  </div>
+
+                  <div>
+                    <h3 className="font-semibold text-slate-800 text-sm">
+                      {compra.titulo}
+                    </h3>
+
+                    <p className="text-sm text-slate-500 text-[11px]">
+                      {compra.data} • {compra.itens} itens
+                    </p>
+                  </div>
+                </div>
+
+                <div className="flex items-center gap-2">
+                  <span className="font-semibold text-green-700">
+                    {compra.valor.toLocaleString("pt-BR", {
+                      style: "currency",
+                      currency: "BRL",
+                    })}
+                  </span>
+
+                  <span className="text-slate-400">
+                    <ChevronRight />
+                  </span>
+                </div>
+              </div>
+            ))}
+          </div> */}
+
           <footer className="fixed bottom-0 left-0 z-50 w-full bg-white px-4 py-2 border-t border-gray-200">
             {" "}
             <div className="flex justify-between mt-2">
